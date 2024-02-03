1f63af52ca9cdde04daf4eb23ec031f2e61d14f7
Adam Walker <adam.walker@virtualmindset.com>
2024-02-03 15:19:19 -0500

    feat: yazi

---
+++ yazi/default.nix
@@ -0,0 +1,17 @@
+{
+  #config, 
+  pkgs
+, # mrc,
+  ...
+}: {
+  # home.packages = with pkgs; [
+  # ];
+  programs = {
+    yazi = {
+      enable = true;
+    };
+  };
+  xdg.configFile."yazi/yazi.toml".source = ./config/yazi.toml;
+  xdg.configFile."yazi/keymap.toml".source = ./config/keymap.toml;
+  xdg.configFile."yazi/theme.toml".source = ./config/theme.toml;
+}
