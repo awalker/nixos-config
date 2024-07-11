c88b3bfb5e136f4dc67af084bb2cfd26e7315bfe
Adam Walker <adam.walker@virtualmindset.com>
2024-07-11 18:07:16 -0400

    changes from odin on Thu Jul 11 06:07:16 PM EDT 2024

--- configuration.nix
+++ configuration.nix
@@ -339,6 +339,7 @@ in
       X11Forwarding = false;
       PermitRootLogin = "prohibit-password";
     };
+    extraConfig = "AllowStreamLocalForwarding true";
   };
   # programs.ssh.enable = true;
   programs.ssh.startAgent = true;
