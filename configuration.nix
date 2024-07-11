c092d9855bd6a82cf1f31cba5dec7d7f048d5184
Adam Walker <adam.walker@virtualmindset.com>
2024-07-11 12:43:40 -0400

    changes from odin on Thu Jul 11 12:43:40 PM EDT 2024

--- configuration.nix
+++ configuration.nix
@@ -326,7 +326,17 @@ in
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    ports = [22];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = ["walke"];
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password";
+    };
+  };
   # programs.ssh.enable = true;
   programs.ssh.startAgent = true;
 
@@ -337,10 +347,10 @@ in
 
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [22];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
