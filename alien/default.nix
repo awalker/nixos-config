fa5570b5f8da1bcfc05590f41b6ee5bcd534c4d2
Adam Walker <adam.walker@virtualmindset.com>
2024-07-11 19:42:59 -0400

    changes from odin on Thu Jul 11 07:42:59 PM EDT 2024

--- alien/default.nix
+++ alien/default.nix
@@ -10,7 +10,7 @@
       system = "x86_64-linux"; # or aarch64-linux
       pkgs = import nixpkgs { inherit system; };
     in {
-        homeConfigurations.odin = home-manager.lib.homeManagerConfiguration rec {
+        homeConfigurations.walke = home-manager.lib.homeManagerConfiguration rec {
           inherit pkgs;
           extraSpecialArgs = { inherit self system; };
           modules = [
