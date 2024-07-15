d89b0d4e038948f15500d27de672f94e51c68325
Adam Walker <adam.walker@virtualmindset.com>
2024-07-15 16:33:20 -0400

    changes from odin on Mon Jul 15 04:33:19 PM EDT 2024

--- configuration.nix
+++ configuration.nix
@@ -336,9 +336,9 @@ in
 # services.xserver.displayManager.sddm.enable = true;
 # services.xserver.desktopManager.plasma5.enable = true;
 
-services.xrdp.enable = true;
-services.xrdp.defaultWindowManager = "Hyprland";
-services.xrdp.openFirewall = true;
+# services.xrdp.enable = true;
+# services.xrdp.defaultWindowManager = "Hyprland";
+# services.xrdp.openFirewall = true;
 
   # Enable the OpenSSH daemon.
   services.openssh = {
