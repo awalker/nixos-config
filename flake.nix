c72e7134d939109acc0dd4502fcffe20a5eb3a2c
Adam Walker <adam.walker@virtualmindset.com>
2024-07-29 19:00:42 -0400

    changes from odin on Mon Jul 29 07:00:42 PM EDT 2024

--- flake.nix
+++ flake.nix
@@ -26,6 +26,7 @@
       user = "walke";
     in
     {
+      home-manager.backupFileExtension = "hm-backup";
       nixosConfigurations = {
         odin = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit user; };
@@ -47,7 +48,6 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.backupFileExtension = "hm-backup";
               home-manager.extraSpecialArgs = {
                 inherit user;
                 system = "x86_64-linux";
