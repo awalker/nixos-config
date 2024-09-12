943d50e5e1e545719e6933221255c14b0a60fdbc
Adam Walker <adam.walker@virtualmindset.com>
2024-09-12 11:16:41 -0400

    changes from odin on Thu Sep 12 11:16:41 AM EDT 2024

--- configuration.nix
+++ configuration.nix
@@ -170,7 +170,7 @@ in
   # services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   # services.xserver.desktopManager.plasma5.enable = true;
   # services.greetd = {
